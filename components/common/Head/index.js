f1098206d97cc6bc8c3add487325790ed7e49a83
MatiasMonasterio <matias_monasterio@outlook.com>
2022-03-01 20:54:11 -0300

    feat: add Head component to SEO

---
+++ components/common/Head/index.js
@@ -0,0 +1,49 @@
+// Test on: https://cards-dev.twitter.com/validator
+// Test on: https://developers.facebook.com/tools/debug/
+// Test on: https://www.linkedin.com/post-inspector/inspect/
+
+// Use meta seo inspector browser extension
+// https://chrome.google.com/webstore/detail/meta-seo-inspector/ibkclpciafdglkjkcibmohobjkcfkaef
+
+import NextHead from "next/head";
+import PropTypes from "prop-types";
+
+export const Head = ({ title, description, image }) => {
+  const metaImage = () => (image ? image : "Default image url");
+
+  return (
+    <NextHead>
+      <title>{title}</title>
+      <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+      <meta charset="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      />
+
+      <meta name="description" content={description} />
+      <meta name="image" content={metaImage} />
+
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:card" content="twitterCard" />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={metaImage} />
+      <meta name="twitter:image:alt" content={title} />
+
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:image" content={metaImage} />
+      <meta name="og:image:secure_url" content={metaImage} />
+      <meta name="og:image:alt" content={title} />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="720" />
+    </NextHead>
+  );
+};
+
+Head.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string,
+};

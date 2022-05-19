ea589cd9521b5f698e2d3d74cabfa4cd07bc2669
MatiasMonasterio <matias_monasterio@outlook.com>
2022-05-19 19:48:06 -0300

    fix: remove div inside ul to use li

--- components/common/Navbar/NavbarDesktop/index.js
+++ components/common/Navbar/NavbarDesktop/index.js
@@ -30,42 +30,46 @@ export const NavbarDesktop = ({ hasScrolled }) => {
         </NavLink>
       </MotionBox>
 
-      <UnorderedList styleType="none" ml="auto">
-        <HStack spacing={6}>
-          <ListItem>
-            <NavLink href="/#experience">Experiencia</NavLink>
-          </ListItem>
+      <UnorderedList
+        styleType="none"
+        ml="auto"
+        display="flex"
+        flexDirection="row"
+        gap={6}
+      >
+        <ListItem>
+          <NavLink href="/#experience">Experiencia</NavLink>
+        </ListItem>
 
-          <ListItem>
-            <NavLink href="/#projects">Proyectos</NavLink>
-          </ListItem>
+        <ListItem>
+          <NavLink href="/#projects">Proyectos</NavLink>
+        </ListItem>
 
-          <ListItem>
-            <NavLink href="/#contact">Contacto</NavLink>
-          </ListItem>
+        <ListItem>
+          <NavLink href="/#contact">Contacto</NavLink>
+        </ListItem>
 
-          <ListItem>
-            <NavLink href="/curriculum">
-              <Box
-                as="span"
-                color="whiteAlpha.900"
-                border="1px solid"
-                rounded="md"
-                px="0.55rem"
-                py="0.55rem"
-                fontWeight="500"
-                borderColor="yellow.200"
-                transition="all 0.25s"
-                _hover={{
-                  bg: "yellow.200",
-                  color: "blackAlpha.900",
-                }}
-              >
-                CV
-              </Box>
-            </NavLink>
-          </ListItem>
-        </HStack>
+        <ListItem>
+          <NavLink href="/curriculum">
+            <Box
+              as="span"
+              color="whiteAlpha.900"
+              border="1px solid"
+              rounded="md"
+              px="0.55rem"
+              py="0.55rem"
+              fontWeight="500"
+              borderColor="yellow.200"
+              transition="all 0.25s"
+              _hover={{
+                bg: "yellow.200",
+                color: "blackAlpha.900",
+              }}
+            >
+              CV
+            </Box>
+          </NavLink>
+        </ListItem>
       </UnorderedList>
     </Container>
   );

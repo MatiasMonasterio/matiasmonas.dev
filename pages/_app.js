9a7337d703335fc2f37e8cc051170c40fbb768ad
MatiasMonasterio <matias_monasterio@outlook.com>
2022-06-22 20:24:41 -0300

    feat: add gtm analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,42 @@
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { AppLayout } from "layouts/AppLayout";
+import { GA_TRACKING_ID, pageview } from "utils/gtag";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => pageview(url);
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <AppLayout {...pageProps}>
         <Component {...pageProps} />
       </AppLayout>

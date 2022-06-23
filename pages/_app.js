830d6b434b88263d1ea36d0506742e392c16e050
MatiasMonasterio <matias_monasterio@outlook.com>
2022-06-23 14:25:48 -0300

    refactor: use appLayout with new export and gtag hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,10 @@
 import Script from "next/script";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
-import { AppLayout } from "layouts/AppLayout";
-import { GA_TRACKING_ID, pageview } from "utils/gtag";
+import AppLayout from "layouts";
+import { useGtagRouterEvents } from "hooks";
 
 function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => pageview(url);
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => router.events.off("routeChangeComplete", handleRouteChange);
-  }, [router.events]);
+  const { GA_TRACKING_ID } = useGtagRouterEvents();
 
   return (
     <>
